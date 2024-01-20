f13aa258b0bb5f00d80fc0df6e89ac9c170b8892
Sahara <sahara.tm65@gmail.com>
2024-01-20 01:00:05 -0500

    Updated transaction table to include user_id

--- Documentation/Database/AccountingLedgerDatabase.sql
+++ Documentation/Database/AccountingLedgerDatabase.sql
@@ -12,7 +12,8 @@ CREATE DATABASE IF NOT EXISTS accounting_ledger;
 use accounting_ledger;
 
 CREATE TABLE transactions (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+	transaction_id INT primary key auto_increment,
+    user_id INT,
     `date` DATE,
     `time` TIME,
     `description` VARCHAR(255),
@@ -22,6 +23,7 @@ CREATE TABLE transactions (
     INDEX idx_date_vendor (`date` , `vendor`),
     INDEX idx_amount (amount)
 );
+
 
 CREATE TABLE users (
     user_id INT NOT NULL AUTO_INCREMENT,
@@ -39,6 +41,7 @@ CREATE TABLE profiles (
     PRIMARY KEY (user_id),
     FOREIGN KEY (user_id)
         REFERENCES users (user_id)
+        ON UPDATE CASCADE
 );
 
 # ---------------------------------------------------------------------- #
@@ -50,12 +53,14 @@ VALUES  ('user','$2a$10$NkufUPF3V8dEPSZeo1fzHe9ScBu.LOay9S3N32M84yuUM2OJYEJ/.','
         ('admin','$2a$10$lfQi9jSfhZZhfS6/Kyzv3u3418IgnWXWDQDk7IbcwlCFPgxg9Iud2','ROLE_ADMIN'),
         ('george','$2a$10$lfQi9jSfhZZhfS6/Kyzv3u3418IgnWXWDQDk7IbcwlCFPgxg9Iud2','ROLE_USER');
         
-INSERT INTO transactions (`date`, `time`, `description`, vendor, amount)
-VALUES  ('2024-01-17', '12:00:00', 'For rent', 'Landlord', 1500.00),
-		('2024-01-18', '15:45:00', 'Service payment', 'ABC Services', 120.75), 
-		('2024-01-18', '17:00:00', 'Office supplies', 'Office Supplies Inc.', 75.20);
+INSERT INTO transactions (user_id, `date`, `time`, `description`, vendor, amount)
+VALUES  (1, '2024-01-17', '12:00:00', 'For rent', 'Landlord', 1500.00),
+		(2, '2024-01-18', '15:45:00', 'Service payment', 'ABC Services', 120.75), 
+		(3, '2024-01-18', '17:00:00', 'Office supplies', 'Office Supplies Inc.', 75.20);
 
 INSERT INTO profiles (user_id, first_name, last_name, email)
 VALUES  (1, 'Christian', 'Escano', 'chris.e@gmail.com'),
         (2, 'Ibrahim', 'Farhane', 'ibrahim.f@gmail.com'),
         (3, 'Sahara', 'Tyner-Melgar', 'sahara.t@gmail.com');
+
+select * from transactions;
