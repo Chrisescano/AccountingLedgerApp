ee2c3b575c87e2d06f138971f31eb796f670ca06
Omar Daoud <omardaoud.dev@gmail.com>
2024-01-22 20:01:40 -0500

    Added Search function

--- AccountingLedgerAPI/accounting-ledger-api/src/main/java/org/yearup/data/TransactionDao.java
+++ AccountingLedgerAPI/accounting-ledger-api/src/main/java/org/yearup/data/TransactionDao.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface TransactionDao {
     Transaction getById(int id);
-    List<Transaction> search(LocalDate start, LocalDate end, String description, String vendor, int max, int min, int userId);
+    List<Transaction> search(LocalDate start, LocalDate end, String description, String vendor, int max, int min, Integer userId);
     List<Transaction> getAll(int userId);
     Transaction addTransaction(Transaction transaction);
 }
